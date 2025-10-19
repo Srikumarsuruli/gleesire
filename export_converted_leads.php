4f4e0d7e9528b556adf2f06ff2bd28fe0f606b74
Srikumarsuruli <146024768+Srikumarsuruli@users.noreply.github.com>
2025-10-19 23:51:09 +0530

    Update export_converted_leads.php

--- export_converted_leads.php
+++ export_converted_leads.php
@@ -9,9 +9,9 @@ if(!isset($_SESSION['id']) && !isset($_SESSION['user_id'])) {
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="converted_leads.csv"');
 
-echo "id,enquiry_id,enquiry_number,customer_name,mobile_number,email\n";
+echo "id,enquiry_id,enquiry_number,customer_name,mobile_number,email,customer_location,secondary_contact,other_details,travel_start_date,travel_end_date,travel_month,night_day,adults_count,children_count,infants_count,children_age_details,lead_type,destination_id,file_manager_id,booking_confirmed,created_at\n";
 
-$sql = "SELECT cl.id, cl.enquiry_id, cl.enquiry_number, e.customer_name, e.mobile_number, e.email
+$sql = "SELECT cl.id, cl.enquiry_id, cl.enquiry_number, e.customer_name, e.mobile_number, e.email, e.customer_location, e.secondary_contact, e.other_details, cl.travel_start_date, cl.travel_end_date, cl.travel_month, cl.night_day, cl.adults_count, cl.children_count, cl.infants_count, cl.children_age_details, cl.lead_type, cl.destination_id, cl.file_manager_id, cl.booking_confirmed, cl.created_at
         FROM converted_leads cl
         JOIN enquiries e ON cl.enquiry_id = e.id
         ORDER BY cl.id";
